bf00db90359119318508d40b53f0e1a14e3daca1
Karen Flores <kareen2707@gmail.com>
2016-05-21 21:07:43 +0200

    Tema 5: Marco de la aplicación

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller'); //Subo a la carpeta routes y accedo a donde esta quiz_controller
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index');
 });
 //Introducimos /question y /ckeck
 router.get('/question', quizController.question);
