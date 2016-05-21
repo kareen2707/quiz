477dd70d34b33d27d19bc1f9a1c6af37f0694889
Karen Flores <kareen2707@gmail.com>
2016-05-21 20:07:44 +0200

    Tema 3: Primera Pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+var quizController = require('../controllers/quiz_controller'); //Subo a la carpeta routes y accedo a donde esta quiz_controller
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+//Introducimos /question y /ckeck
+router.get('/question', quizController.question);
+router.get('/check', quizController.check);
 
 module.exports = router;
