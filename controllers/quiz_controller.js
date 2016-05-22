44d867d78c68749595b6d044639518a010de46a1
Karen Flores <kareen2707@gmail.com>
2016-05-22 15:34:03 +0200

    Tema 8: sequelize.js ySQlite

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,14 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
+var Sequelize = require('sequelize');
 //GET /question
 exports.question = function (req, res, next){
-	var answer = req.query.answer || '';
-	res.render('quizzes/question',{question: 'Capital de Italia', answer : answer});
+	models.Quiz.findOne() //Busca la primera pregunta
+	.then(function(quiz){
+		if(quiz){
+			var answer = req.query.answer || '';
+			res.render('quizzes/question',{question: quiz.question, answer : answer});
+		}
+		else{
+			throw new Error ('No hay preguntas en la BBDD.');
+		}
+	}).catch(function(error){ next(error);});	
 };
 
 //GET //check
 exports.check = function(req, res, next) {
-	var answer = req.query.answer || '';
-	var result = ((answer === 'Roma') ? 'Correcta' : 'Incorrecta');
-	res.render('quizzes/result', {result : result, answer: answer});
+	models.Quiz.findOne()
+	.then(function(quiz){
+		if(quiz){
+		var answer = req.query.answer || '';
+		var result = answer===quiz.answer ? 'Correcta' : 'Incorrecta';
+		res.render('quizzes/result', {result : result, answer: answer});	
+		}
+		else{
+			throw new Error ('No hay preguntas en la BBDD.');
+		}
+	}).catch(function(error){next(error);});	
 };
---
+++ models/index.js
@@ -0,0 +1,36 @@
+var path = require ('path');
+
+//Cargar Modelo ORM
+var Sequelize = require("sequelize");
+
+//Usar BBDD Sqlite
+var sequelize = new Sequelize(null, null, null, {
+dialect: "sqlite",
+storage: "quiz.sqlite"
+});
+
+//Importar la definición de la tabla Quiz de quiz.js
+var Quiz = sequelize.import(path.join(__dirname, "quiz"));
+
+//sequelize.sync() crea e inicializa tabla de preguntas en DB
+sequelize.sync().then(function() {
+	return Quiz.count().then(function(c) {
+		if (c === 0) {
+		return Quiz.bulkCreate([{
+			question: "Capital de Italia",
+			answer: "Roma"
+			}, 
+			{
+			question: "Capital de Portugal",
+			answer: "Lisboa"
+			}]).then(function() {
+console.log("Base de datos inicialzada con datos");
+});
+}
+});
+}).catch(function(error) {
+console.log("Error Sincronizando las tablas de la BD", error);
+progress.exit(1);
+});
+
+exports.Quiz = Quiz; //exporta la deifinición de la tabla Quiz
