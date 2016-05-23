1be8236cb7999a333476106c7a0e1c2572126b59
Karen Flores <kareen2707@gmail.com>
2016-05-23 11:24:29 +0200

    Tema 15:Editar preguntas

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var partials = require('express-partials');
 var flash = require('express-flash');
+var methodOverride = require('method-override');
 var routes = require('./routes/index');
 
 var app = express();
@@ -15,8 +16,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(partials());
-app.use(flash());
+
+
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -25,7 +26,10 @@ app.use(cookieParser());
 app.use(session({secret: "Quiz 2016", //semilla del cifrado de cookies
                 resave: false,
                 saveUninitialized: true}));
+app.use(methodOverride('_method',{methods:["POST","GET"]}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(partials());
+app.use(flash());
 
 app.use('/', routes);
 
