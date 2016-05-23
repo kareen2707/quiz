af41bc9f1e649f25bbde8cf664012f7114b31708
Karen Flores <kareen2707@gmail.com>
2016-05-23 13:10:41 +0200

    P9: Créditos

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quiz_controller'); //Subo a la carpeta routes y accedo a donde esta quiz_controller
-/* GET home page. */
+// GET home page. 
 router.get('/', function(req, res) {
   res.render('index');
 });
-//Introducimos /question y /ckeck
-/*router.get('/question', quizController.question);
-router.get('/check', quizController.check);*/
+
+//GET author page.
+router.get('/author', function(req,res){
+	res.render('author');
+});
 
 //Autoload de rutas que usen :quizId
 router.param('quizId',quizController.load);
