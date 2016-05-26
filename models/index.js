937b2dffb4ce49fc66c39294d78d70eea44224de
Karen Flores <kareen2707@gmail.com>
2016-05-26 19:33:59 +0200

    Tema 25: Roles y Permisos. Heroku

--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@ var path = require('path');
 var Sequelize = require("sequelize");
 
 //Usar BBDD Sqlite en Heroku
-/*var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
 
  var DATABASE_PROTOCOL = url[1];
  var DATABASE_DIALECT = url[1];
@@ -21,13 +21,13 @@ var Sequelize = require("sequelize");
      host: DATABASE_HOST,
      storage: DATABASE_STORAGE,
      omitNULL: true
- });*/
+ });
 
 //Usar BBDD Sqlite en mi máquina
-var sequelize = new Sequelize(null, null, null, {
+/*var sequelize = new Sequelize(null, null, null, {
     dialect: "sqlite",
     storage: "quiz.sqlite"
-});
+});*/
 
 //Importar la definición de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname, "quiz"));
